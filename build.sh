759c2168b0df78c9981ca83483afb08e1af28998
Shubham Chaudhary <shubhamchaudhary1996@gmail.com>
2020-10-30 19:42:28 +0530

    Updated build script.

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ git init
 git remote add origin https://github.com/5hubh4m/5hubh4m.github.io
 git add -A
 git commit -m "Publish."
-git push origin master
+git push origin master -f
 cd ..
+rm -rf $TMP
