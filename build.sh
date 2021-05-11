b656a80ccdcaf5e5fd96058ce6164c989903a081
Shubham Chaudhary <shubhamchaudhary1996@gmail.com>
2021-05-11 16:41:13 -0400

    Move to cornell servers.

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-TMP=$(mktemp -d)
 JEKYLL_ENV=production bundle exec jekyll b --safe
-cp -r _site/. $TMP
-cd $TMP
-git init
-git remote add origin https://github.com/5hubh4m/5hubh4m.github.io
-git add -A
-git commit -m "Publish."
-git push origin master -f
-cd ..
-rm -rf $TMP
+rsync -avzhp --exclude .DS_Store --delete _site/ linux.coecis.cornell.edu:/cs/people/sc2937
